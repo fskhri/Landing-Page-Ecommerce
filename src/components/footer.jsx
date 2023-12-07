c918961c7736f103ee7ab553987e0b7a611c94d1
fskhri <fakhrijuanda88@gmail.com>
2023-12-07 08:47:49 +0700

    cleaning up unusefull code 	modified:   src/components/footer.jsx 	modified:   src/components/product-section.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,31 +1,50 @@
+import React from 'react';
+
 function Footer() {
-    return (
-        <div className="bg-black">
-            <div className="p-4 flex flex-wrap justify-center gap-10">
-                <div className="p-0">
-                    <p className="text-lg text-slate-100">Address</p>
-                    <p className="text-sm text-slate-200">Jl Lorem ipsum dolor sit amet. jakarta Indonesia</p>
-                </div>
-                <div className="p-0">
-                    <p className="text-lg text-slate-100">Contact Us</p>
-                    <ul className="list-none">
-                        <li className="text-slate-200 hover:text-blue-300">Instagram</li>
-                        <li className="text-slate-200 hover:text-blue-300">Telegram</li>
-                        <li className="text-slate-200 hover:text-blue-300">Email</li>
-                    </ul>
-                </div>
-                <div className="p-0">
-                    <p className="text-lg text-slate-100">Message us</p>
-                    <form action="" method="post" className="my-3">
-                        <input type="email" name="email" id="" className="block gap-2 p-2 border-none rounded" placeholder="Your email"/>
-                        <input type="password" name="password" className="block my-2 p-2 border-none rounded" id=""  placeholder="Password"/>
-                        <button type="submit" className="py-2 px-20 bg-blue-400 text-white rounded hover:bg-blue-300">Send</button>
-                    </form>
-                </div>
-            </div>
-            <p className="p-5 text-slate-300 text-center">Created with ❤️ <span className="text-blue-500">Mangandaralam Sakti</span> 2023 </p>
+  return (
+    <div className="bg-black text-white">
+      <div className="p-4 flex flex-wrap justify-center gap-10">
+        <div className="p-0">
+          <p className="text-lg font-bold text-slate-100">Address</p>
+          <p className="text-sm text-slate-200">Jl Lorem ipsum dolor sit amet, Jakarta, Indonesia</p>
         </div>
-    )
+        <div className="p-0">
+          <p className="text-lg font-bold text-slate-100">Contact Us</p>
+          <ul className="list-none">
+            <li className="text-slate-200 hover:text-blue-300">Instagram</li>
+            <li className="text-slate-200 hover:text-blue-300">Telegram</li>
+            <li className="text-slate-200 hover:text-blue-300">Email</li>
+          </ul>
+        </div>
+        <div className="p-0">
+          <p className="text-lg font-bold text-slate-100">Message us</p>
+          <form action="" method="post" className="my-3">
+            <input
+              type="email"
+              name="email"
+              className="block w-full p-2 border-none rounded"
+              placeholder="Your email"
+            />
+            <input
+              type="password"
+              name="password"
+              className="block w-full my-2 p-2 border-none rounded"
+              placeholder="Password"
+            />
+            <button
+              type="submit"
+              className="py-2 px-6 bg-blue-400 text-white rounded hover:bg-blue-300"
+            >
+              Send
+            </button>
+          </form>
+        </div>
+      </div>
+      <p className="p-5 text-slate-300 text-center">
+        Created with ❤️ by <span className="text-blue-500">Mangandaralam Sakti</span> 2023
+      </p>
+    </div>
+  );
 }
 
 export default Footer;
--- src/components/product-section.jsx
+++ src/components/product-section.jsx
@@ -2,10 +2,9 @@ import React, { useState } from 'react';
 import ListPic from '../assets/vivo.jpeg';
 
 function Product() {
-  const [showMoreAll, setShowMoreAll] = useState(false);
   const [showMoreBestSeller, setShowMoreBestSeller] = useState(false);
 
-  const products = [
+  const bestSellerProducts = [
     { name: 'Vivo', id: 1 },
     { name: 'Oppo', id: 2 },
     { name: 'Samsung', id: 3 },
@@ -15,46 +14,42 @@ function Product() {
     { name: 'Rog', id: 7 },
   ];
 
-  const bestSellerProducts = [
-    { name: 'Rog', id: 4 },
-    { name: 'Iphone 12', id: 5 },
-    { name: 'Samsung Z flip', id: 6 },
-    { name: 'Rog', id: 7 },
-  ];
+  const renderProductSection = (sectionProducts, sectionTitle, showMore, setShowMore) => {
+    const maxItemsToShow = showMore ? sectionProducts.length : 4;
 
-  const renderProductSection = (sectionProducts, sectionTitle, showMore, setShowMore) => (
-    <div className="my-8">
-      <h2 className="text-2xl mb-4">{sectionTitle}</h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        {sectionProducts.map((product) => (
-          <div key={product.id} className="bg-white p-4 rounded shadow-md">
-            <img
-              src={ListPic}
-              alt="list-img"
-              className="w-full h-32 object-contain mb-2"
-            />
-            <p className="text-center">{product.name}</p>
-          </div>
-        ))}
-      </div>
-      {!showMore && (
-        <div className="text-center my-4">
-          <button
-            onClick={() => setShowMore(true)}
-            className="py-2 px-3 rounded bg-blue-400 text-white"
-          >
-            Read More {sectionTitle}
-          </button>
+    return (
+      <div className="my-8">
+        <h2 className="text-2xl mb-4">{sectionTitle}</h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          {sectionProducts.slice(0, maxItemsToShow).map((product) => (
+            <div key={product.id} className="bg-white p-4 rounded shadow-md">
+              <img
+                src={ListPic}
+                alt="list-img"
+                className="w-full h-32 object-contain mb-2"
+              />
+              <p className="text-center">{product.name}</p>
+            </div>
+          ))}
         </div>
-      )}
-    </div>
-  );
+        {sectionProducts.length > 3 && !showMore && (
+          <div className="text-center my-4">
+            <button
+              onClick={() => setShowMore(true)}
+              className="py-2 px-3 rounded bg-blue-400 text-white"
+            >
+              Read More {sectionTitle}
+            </button>
+          </div>
+        )}
+      </div>
+    );
+  };
 
   return (
     <div className="my-8 p-2">
-      {renderProductSection(products.slice(0, 3), 'All Products', showMoreAll, setShowMoreAll)}
       {renderProductSection(
-        bestSellerProducts.slice(0, 3),
+        bestSellerProducts,
         'Best Seller Products',
         showMoreBestSeller,
         setShowMoreBestSeller
