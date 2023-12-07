64afed963d0053232ad7f2750afdc3f5cda3bb6f
fskhri <fakhrijuanda88@gmail.com>
2023-12-07 08:55:18 +0700

    add router and add some package 	modified:   package.json 	modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,27 @@
-import { useState } from 'react'
-import './App.css'
-import Navbar from './components/navbar'
-import Home from './components/home-section'
-import Product from './components/product-section'
-import OurService  from './components/service-section'
-import Footer from './components/footer'
+// App.jsx
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/navbar';
+import Home from './components/home-section';
+import Product from './components/product-section';
+import OurService from './components/service-section';
+import Footer from './components/footer';
 
 function App() {
-
   return (
-    <>
-      <Navbar/>
-      <Home/>
-      <Product/>
-      <OurService/>
-      <Footer/>
-    </>
-  )
+    <Router>
+      <div>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/products" component={Product} />
+          <Route path="/services" component={OurService} />
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
