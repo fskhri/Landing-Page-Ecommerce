7eeadb7fe4485fa0edd0e41577653ab52aff8f17
fskhri <fakhrijuanda88@gmail.com>
2023-12-07 10:21:09 +0700

    fixed footer 	modified:   src/components/footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Footer() {
   return (
-    <div className="my-[182px] bg-black text-white">
+    <div className="fixed bottom-0 left-0 w-full bg-black text-white">
       <div className="p-4 flex flex-wrap justify-center gap-10">
         <div className="p-0">
           <p className="text-lg font-bold text-slate-100">Address</p>
