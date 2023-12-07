a562700b9d7b0569cfa8cff0571f7acebda92046
fskhri <fakhrijuanda88@gmail.com>
2023-12-07 08:15:42 +0700

    menambahkan readmore katagori 	modified:   src/components/product-section.jsx

--- src/components/product-section.jsx
+++ src/components/product-section.jsx
@@ -2,10 +2,11 @@ import React, { useState } from 'react';
 import ListPic from '../assets/vivo.jpeg';
 
 function Product() {
-  const [showMore, setShowMore] = useState(false);
+  const [showMoreAll, setShowMoreAll] = useState(false);
+  const [showMoreBestSeller, setShowMoreBestSeller] = useState(false);
 
   // Define your product data here
-  const products = [
+  const allProducts = [
     { name: 'Vivo', id: 1 },
     { name: 'Oppo', id: 2 },
     { name: 'Samsung', id: 3 },
@@ -15,32 +16,44 @@ function Product() {
     { name: 'Rog', id: 7 },
   ];
 
-  // Adjust the number of products to show based on the showMore state
-  const visibleProducts = showMore ? products : products.slice(0, 3);
+  const bestSellerProducts = [
+    { name: 'Rog', id: 4 },
+    { name: 'Iphone 12', id: 5 },
+    { name: 'Samsung Z flip', id: 6 },
+    { name: 'Rog', id: 7 },
+  ];
 
-  return (
-    <div className="my-8 p-2">
-      <div className="flex flex-wrap justify-center gap-2">
-        {visibleProducts.map((product) => (
-          <div key={product.id}>
-            <img src={ListPic} alt="list-img" />
-            <p className="flex justify-center p-2">{product.name}</p>
-          </div>
-        ))}
-      </div>
-
+  const renderProductSection = (sectionProducts, sectionTitle, showMore, setShowMore) => (
+    <div className="my-8 flex flex-wrap justify-center gap-2">
+      {sectionProducts.map((product) => (
+        <div key={product.id}>
+          <img src={ListPic} alt="list-img" />
+          <p className="flex justify-center p-2">{product.name}</p>
+        </div>
+      ))}
       {!showMore && (
         <div className="text-center my-4">
           <button
             onClick={() => setShowMore(true)}
             className="py-2 px-3 rounded bg-blue-400 text-white"
           >
-            Read More
+            Read More {sectionTitle}
           </button>
         </div>
       )}
+    </div>
+  );
 
-      {/* Add your "Best seller Product" section here */}
+  return (
+    <div className="my-8 p-2">
+      {renderProductSection(allProducts.slice(0, 3), 'All Products', showMoreAll, setShowMoreAll)}
+
+      {renderProductSection(
+        bestSellerProducts.slice(0, 3),
+        'Best Seller Products',
+        showMoreBestSeller,
+        setShowMoreBestSeller
+      )}
     </div>
   );
 }
