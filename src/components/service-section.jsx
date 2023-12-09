d5a1ea15c41f0ac8b5b1dcfb778441cb246c2c3d
fskhri <fakhrijuanda88@gmail.com>
2023-12-10 05:32:59 +0700

    add animation left right to services 	modified:   src/components/service-section.jsx

--- src/components/service-section.jsx
+++ src/components/service-section.jsx
@@ -1,31 +1,32 @@
 function OurService() {
     return (
-        <div className="my-3">
-            <h2 className="text-center text-2xl font-bold">Our Services</h2>
-            <div className="my-4 flex flex-wrap justify-center gap-8">
-                <div className="shadow-md rounded p-2 w-50">
-                    <p className="text-lg font-bold">Garansi</p>
-                    <p className="my-2">Kami menyediakan garansi barang selama 2 tahun dimulai saat pembelian barang
-                    hingga garansi uang kembali jika pelanggan mendapat barang rusak saat membeli</p>
-                </div>
-                <div className="shadow-md rounded p-2 w-50">
-                    <p className="text-lg font-bold">Garansi</p>
-                    <p className="my-2">Kami menyediakan garansi barang selama 2 tahun dimulai saat pembelian barang
-                    hingga garansi uang kembali jika pelanggan mendapat barang rusak saat membeli</p>
-                </div>
-                <div className="shadow-md rounded p-2 w-50">
-                    <p className="text-lg font-bold">Garansi</p>
-                    <p className="my-2">Kami menyediakan garansi barang selama 2 tahun dimulai saat pembelian barang
-                    hingga garansi uang kembali jika pelanggan mendapat barang rusak saat membeli</p>
-                </div>
-                <div className="shadow-md rounded p-2 w-50">
-                    <p className="text-lg font-bold">Garansi</p>
-                    <p className="my-2">Kami menyediakan garansi barang selama 2 tahun dimulai saat pembelian barang
-                    hingga garansi uang kembali jika pelanggan mendapat barang rusak saat membeli</p>
-                </div>
-            </div>
+      <div className="my-3">
+        <h2 className="text-center text-2xl font-bold">Our Services</h2>
+        <div className="my-4 flex flex-wrap justify-center gap-8">
+          <div className="shadow-md rounded p-4 w-50 transform transition-transform duration-500 hover:translate-x-2">
+            <p className="text-lg font-bold">Garansi</p>
+            <p className="my-2">Kami menyediakan garansi barang selama 2 tahun dimulai saat pembelian barang
+              hingga garansi uang kembali jika pelanggan mendapat barang rusak saat membeli</p>
+          </div>
+          <div className="shadow-md rounded p-4 w-50 transform transition-transform duration-500 hover:translate-x-2">
+            <p className="text-lg font-bold">Garansi</p>
+            <p className="my-2">Kami menyediakan garansi barang selama 2 tahun dimulai saat pembelian barang
+              hingga garansi uang kembali jika pelanggan mendapat barang rusak saat membeli</p>
+          </div>
+          <div className="shadow-md rounded p-4 w-50 transform transition-transform duration-500 hover:translate-x-2">
+            <p className="text-lg font-bold">Garansi</p>
+            <p className="my-2">Kami menyediakan garansi barang selama 2 tahun dimulai saat pembelian barang
+              hingga garansi uang kembali jika pelanggan mendapat barang rusak saat membeli</p>
+          </div>
+          <div className="shadow-md rounded p-4 w-50 transform transition-transform duration-500 hover:translate-x-2">
+            <p className="text-lg font-bold">Garansi</p>
+            <p className="my-2">Kami menyediakan garansi barang selama 2 tahun dimulai saat pembelian barang
+              hingga garansi uang kembali jika pelanggan mendapat barang rusak saat membeli</p>
+          </div>
         </div>
-    )
-}
-
-export default OurService
+      </div>
+    );
+  }
+  
+  export default OurService;
+  
